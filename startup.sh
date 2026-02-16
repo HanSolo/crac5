1005f4043a0d967f763a5110f312cfc6062f6cf4
Gerrit Grunwald <han.solo@mac.com>
2026-02-16 17:33:21 +0100

    tmp

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sleep 45; jcmd /opt/app/crac5-25.0.0.jar JDK.checkpoint & java -XX:CRaCEngine=warp -XX:CRaCCheckpointTo=/opt/crac-files -XX:CPUFeatures=ignore -jar /opt/app/crac5-25.0.0.jar && fg
+sleep 45; jcmd /opt/app/crac5-25.0.0.jar JDK.checkpoint &
+java -XX:CRaCEngine=warp -XX:CRaCCheckpointTo=/opt/crac-files -XX:CPUFeatures=ignore -jar /opt/app/crac5-25.0.0.jar &
